b4d2cdad7f81c8dd00640ffc953bb205d3d83f76
Rajesh <rajeshkumarraje9@gmail.com>
2021-11-09 13:21:24 +0530

    css adding commit

--- FuelApp/views.py
+++ FuelApp/views.py
@@ -1,7 +1,11 @@
-from django.http.response import HttpResponse
-
+from django.http.response import HttpResponse, HttpResponseNotFound
+from django.template.loader import render_to_string
 
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<center><h1>Please check the url in the petrol app</h1></center>")
+    try:
+        return HttpResponse("<center><h1>Please check the url in the petrol app</h1></center>")
+    except:
+        repsonse_data = render_to_string("404.html")
+        return HttpResponseNotFound(repsonse_data)
--- petrol/views.py
+++ petrol/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import HttpResponse, HttpResponseNotFound
+from django.template.loader import render_to_string
 # Create your views here.
+
 def index(request):
-    return render(request,"petrol/index.html")
-    # return HttpResponse("Hello World!")
+    try:
+        return render(request,"petrol/index.html")
+
+    except:
+        repsonse_data = render_to_string("404.html")
+        return HttpResponseNotFound(repsonse_data)
