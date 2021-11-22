7470ce9ef26fc0c988e028ee7e851864de7ed35c
Rajesh <rajeshkumarraje9@gmail.com>
2021-11-22 20:10:11 +0530

    password check

--- FuelApp/settings.py
+++ FuelApp/settings.py
@@ -150,7 +150,7 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER ='rajeshtest77@gmail.com'
-EMAIL_HOST_PASSWORD = 'Test9@90'
+EMAIL_HOST_PASSWORD = 'TypeyourpasswordHere'
 EMAIL_USE_TLS = True
 
 # custom user level login
